19514ba2ce0ef7da3f454125d89da3e9f487ccd3
ahmed-soltan <soa548277@gmail.com>
2024-06-16 19:59:48 +0300

    fixing some issues with search functionality

--- actions/getCourses.ts
+++ actions/getCourses.ts
@@ -21,6 +21,7 @@ export const GetCourses = async ({ userId, title, categoryId }: GetCourses) => {
         isPublished: true,
         title: {
             contains: title,
+            mode: 'insensitive'
         },
         categoryId
       },
